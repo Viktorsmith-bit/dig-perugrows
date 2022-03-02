6dd25d60ffc2802cf3ae9155598989552623acb5
Victorsmith <victorsmith@Victorsmiths-MacBook-Air.local>
2022-03-02 15:42:43 -0500

    commit 25 perugrows

--- src/components/visor/components/capas/components/capas.js
+++ src/components/visor/components/capas/components/capas.js
@@ -11,8 +11,8 @@ export default function ListCapas(props){
     const hundleCloseGlacLagu = (e)=>{e.preventDefault(), setGlacLagu(true)}
 
     return(
-        <div className='absolute-capas bg-white w-full md:w-96 lg:w-96 rounded-sm border border-gray-400 height-visor ' style={{zIndex:"1000"}}>
-            <div className='flex items-center justify-between bg-green-700 px-4 h-10 rounded-t-sm'>
+        <div className='absolute-capas bg-white w-full md:w-96 lg:w-96.5 rounded-sm border border-gray-400 height-visor ' style={{zIndex:"1000"}}>
+            <div className='flex items-center justify-between bg-green-700 px-3 h-10 rounded-t-sm'>
                 <div className='flex items-center gap-2'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-stack" viewBox="0 0 16 16">
                         <path d="m14.12 10.163 1.715.858c.22.11.22.424 0 .534L8.267 15.34a.598.598 0 0 1-.534 0L.165 11.555a.299.299 0 0 1 0-.534l1.716-.858 5.317 2.659c.505.252 1.1.252 1.604 0l5.317-2.66zM7.733.063a.598.598 0 0 1 .534 0l7.568 3.784a.3.3 0 0 1 0 .535L8.267 8.165a.598.598 0 0 1-.534 0L.165 4.382a.299.299 0 0 1 0-.535L7.733.063z"/>
@@ -23,7 +23,7 @@ export default function ListCapas(props){
                 <Close handleClickClose={props.handleClickClose} />
             </div>
             <div className='overFlow-hidden h-96'>
-                <div className='overflow-auto overscroll-auto h-96 py-3 px-4'>
+                <div className='overflow-auto overscroll-auto h-96 p-3'>
                     <h1 className='text-base text-color border-b-2 border-gray-600 pb-1'>Capas</h1>
                     <div className='flex gap-2 pt-2'>
                         <div className='flex gap-2'>
